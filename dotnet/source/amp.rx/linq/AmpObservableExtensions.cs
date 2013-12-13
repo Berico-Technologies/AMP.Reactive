07a91b94ccd8a43be6eadf47773e431a5d60698f
Jared Holmberg <jholmberg@bericotechnologies.com>
2013-12-12 23:56:33 -0500

    Initial skeleton framework to create an observable that will do pub/sub with AMPere.

---
+++ dotnet/source/amp.rx/linq/AmpObservableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using amp.rx.eventing;
+using cmf.bus;
+using cmf.eventing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace amp.rx.linq
+{
+    public partial class AmpObservableExtensions
+    {
+        public static IObservable<TEvent> FromAmpEventPattern<TEvent>(this IEventBus eventBus, Func<TEvent, IDictionary<string, string>, object> eventHandler, string topic)
+        {
+            return FromAmpEventPattern<TEvent>(eventBus, eventHandler, topic, null);
+        }
+
+        public static IObservable<TEvent> FromAmpEventPattern<TEvent>(this IEventBus eventBus, Func<TEvent, IDictionary<string, string>, object> eventHandler, string topic, Func<Envelope, Exception, object> failedHandler)
+        {
+            //Register a subscriber of TEvent
+            ReactiveEventHandler<TEvent> handler = new ReactiveEventHandler<TEvent>(eventHandler, failedHandler, topic);
+            //Add handler
+            eventBus.Subscribe(handler);
+
+            return null;
+        }
+    }
+}
